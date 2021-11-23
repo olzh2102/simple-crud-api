f134f3f32c9e0d8b7c28ebb111d79d4ea8bceaf6
Olzhas Kurikov <okur@hellofresh.com>
2021-11-23 16:52:44 +0100

    feat: fake in-memory database

---
+++ data.js
@@ -0,0 +1,16 @@
+const persons = [
+    {
+        id: 1,
+        name: 'Nurlan',
+        age: 30,
+        hobbies: ['boxing', 'standup']
+    },
+    {
+        id: 2,
+        name: 'Azamat',
+        age: 38,
+        hobbies: ['reading', 'hunting']
+    }
+]
+
+module.exports = persons
