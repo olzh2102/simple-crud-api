ba654f7f4f710f301b4a160e8c0a442301f4e23d
Olzhas Kurikov <okur@hellofresh.com>
2021-11-23 17:43:10 +0100

    feat: simple GET person by id

--- app.js
+++ app.js
@@ -6,22 +6,38 @@ const PORT = process.env.PORT || 5000
 
 const server = http.createServer(
     async (req, res) => {
-        if (req.url == '/api/persons' && req.method == 'GET') {
+        // * /api/persons : GET
+        if (
+            req.url === '/api/persons' && 
+            req.method == 'GET'
+        ) {
             const persons = await new Persons().getPersons()
             res.writeHead(
                 200,
                 {'Content-Type': 'application/json'}
             )
             res.end(JSON.stringify(persons))
-        } else {
-            res.writeHead(
-                404,
-                {'Content-Type': 'application/json'}
-            )
-            
-            res.end(
-                JSON.stringify({ message: 'Route not found' })
-            )
+        } 
+
+        else if (
+            req.url.match(/\/api\/persons\/([0-9]+)/) &&
+            req.method == 'GET'
+        ) {
+            try {
+                const id = req.url.split('/')[3]
+                const person = await new Persons().getPersons(id)
+                res.writeHead(200, {'Content-Type': 'application/json'})
+                res.end(JSON.stringify(person))
+            } catch (error) {
+                res.writeHead(404, {'Content-Type': 'application/json'})
+                res.end(JSON.stringify({ message: error }))
+            }
+        }
+
+        // * Route does not exist
+        else {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Route not found" }));
         }
     }
 )
--- controller.js
+++ controller.js
@@ -5,6 +5,18 @@ class PersonsController {
     async getPersons() {
         return new Promise((resolve) => resolve(persons))
     }
+
+    // * get persons
+    async getPersons(id) {
+        return new Promise((resolve, reject) => {
+            let person = persons.find((p) => p.id == parseInt(id))
+
+            if (person)
+                resolve(person)
+            else
+                reject(`Person with id ${id} not found.`)
+        })
+    }
 }
 
 module.exports = PersonsController
